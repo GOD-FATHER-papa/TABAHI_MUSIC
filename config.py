2dd33a79fb984143975caaee9e4c3c44f7e762bd
RONEY THE BRAND <nikhilkohli928877@gmail.com>
2026-08-24 23:30:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ LOGGER_ID = int(getenv("LOGGER_ID", -1003670001038))
 
 # Vars For API End Pont.
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
-YT_API_KEY = getenv("YT_API_KEY" , 'ShrutiBotsTUOhRD5u1C8vTXPfFZys') ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
+YT_API_KEY = getenv("YT_API_KEY" , 'AIzaSyBZP0V66yZivXCZNGC1V7uloqrjgRTe53w') ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
 
 # ======================================================
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
